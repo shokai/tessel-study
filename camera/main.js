a663a6317bbecc3288422602d6eb96a44cd11421
Sho Hashimoto <hashimoto@shokai.org>
2014-10-05 11:50:12 +0900

    blink_led
    camera
    
    yo
    
    http-tiny-router
    
    http-server
    
    camera-server
    
    rfid

--- camera/main.js
+++ camera/main.js
@@ -1,10 +1,17 @@
 var tessel = require('tessel');
 var camera = require('camera-vc0706').use(tessel.port['A']);
 
-var notificationLED = tessel.led[3];
+var led_green = tessel.led[0];
 
 camera.on('ready', function() {
-  notificationLED.high();
+  camera.setResolution('vga', function(err, res){
+    if(err) throw 'setting camera resolution failed!';
+    camera.emit('ready:capture');
+  });
+});
+
+camera.on('ready:capture', function(){
+  led_green.high();
 
   camera.takePicture(function(err, image) {
     if(err){
@@ -12,7 +19,7 @@ camera.on('ready', function() {
       return;
     }
 
-    notificationLED.low();
+    led_green.low();
     var name = 'picture-' + Math.floor(Date.now()*1000) + '.jpg';
     console.log('Picture saving as', name, '...');
     process.sendfile(name, image);
---
+++ yo/yo.js
@@ -0,0 +1,23 @@
+var tessel = require('tessel');
+var wifi   = require('wifi-cc3000');
+var Yo     = require('yo-api');
+
+var yo_api_key = process.argv[2];
+var yo = new Yo(yo_api_key);
+
+var led_green = tessel.led[0].output(1);
+setInterval(function(){
+  if(wifi.isConnected()) led_green.toggle()
+}, 500);
+
+// wifiを再起動
+wifi.reset();
+
+wifi.on('connect', function(){
+  console.log('wifi connect');
+  yo.yo('SHOKAI', function(err, res, body){
+    console.log(err);
+    console.log(body.toString());
+  });
+});
+
