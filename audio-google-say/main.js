e005f56bee7c043bffdb9428f9e547df5a7b9ac2
Sho Hashimoto <hashimoto@shokai.org>
2014-10-12 00:17:49 +0900

    google say  #5

--- audio-google-say/main.js
+++ audio-google-say/main.js
@@ -1,14 +1,15 @@
 var tessel = require('tessel');
 var fs = require('fs');
 var request = require('request');
+var wifi   = require('wifi-cc3000');
 var audio = require('audio-vs1053b').use(tessel.port['A']);
 
 var led_green = tessel.led[0].output(1);
 setInterval(function(){
   led_green.toggle()
-}, 500);
+}, 200);
 
-var getAudio = module.exports = function(speech){
+var getAudioStream = module.exports = function(speech){
   return request.get({
     uri: 'http://translate.google.com/translate_tts',
     qs: {
@@ -18,14 +19,25 @@ var getAudio = module.exports = function(speech){
     headers: {
       'User-Agent': 'Safari/1.0'
     }
-  })
+  });
 };
 
+var say = function(speech){
+  getAudioStream(speech).pipe(audio.createPlayStream());
+};
 
 audio.on('ready', function(){
   console.log('audio ready');
+  wifi.reset();
+});
+
+wifi.on('connect', function(){
+  console.log('wifi connect');
   audio.setVolume(20, function(err){
     if(err) return console.error(err);
-    getAudio('ぺーろぺろ').pipe(audio.createPlayStream());
+    setInterval(function(){
+      say('よお');
+    }, 1000*30);
+    say('よお');
   });
 });
